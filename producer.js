cf24a5522d0b04e54c216188064226de42cdc7a0
Marcelo Mont-Morency Gondim Rodrigues <morencymarcelo@gmail.com>
2024-11-13 14:50:03 -0300

    colocando o valor enviado

--- producer.js
+++ producer.js
@@ -1,4 +1,4 @@
-const amqp = require("amqplib");
+/*const amqp = require("amqplib");
 const dotenv = require("dotenv").config();
 
 let api_key = process.env.AMQP;
@@ -25,6 +25,36 @@ async function sendMessage() {
   } catch (error) {
     console.error(error);
     connection.close();
+  }
+}
+
+sendMessage();
+*/
+
+const amqp = require("amqplib");
+const dotenv = require("dotenv").config();
+
+let api_key = process.env.AMQP;
+
+async function sendMessage() {
+  let queue = "hello";
+  let message = JSON.stringify({ value: 40.0 }); // Valor de dinheiro a ser enviado
+
+  try {
+    const connection = await amqp.connect(api_key);
+    const channel = await connection.createChannel();
+
+    await channel.assertQueue(queue, { durable: false });
+    channel.sendToQueue(queue, Buffer.from(message));
+
+    console.log("Valor enviado: " + message);
+
+    setTimeout(() => {
+      connection.close();
+      process.exit(0);
+    }, 500);
+  } catch (error) {
+    console.error(error);
   }
 }
 
