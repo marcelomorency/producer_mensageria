603cd489011fa7995db47ffc1a7c22a1130617cd
Marcelo Mont-Morency Gondim Rodrigues <morencymarcelo@gmail.com>
2024-11-13 11:20:11 -0300

    primeiro commit

---
+++ producer.js
@@ -0,0 +1,31 @@
+const amqp = require("amqplib");
+const dotenv = require("dotenv").config();
+
+let api_key = process.env.AMQP;
+
+async function sendMessage() {
+  let queue = "hello";
+
+  let message = "Hello World";
+
+  try {
+    const connection = await amqp.connect(api_key);
+
+    const channel = await connection.createChannel();
+
+    await channel.assertQueue(queue, { durable: false });
+
+    channel.sendToQueue(queue, Buffer.from(message));
+    console.log("Mensagem enviada: " + message);
+
+    setTimeout(() => {
+      connection.close();
+      process.exit(0);
+    }, 500);
+  } catch (error) {
+    console.error(error);
+    connection.close();
+  }
+}
+
+sendMessage();
